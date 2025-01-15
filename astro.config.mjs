fc8efaa479c6e502d7679d50082aa240cba973d9
3urdparty <3urdparty@gmail.com>
2025-01-15 22:21:55 +0800

    Initialized project

---
+++ astro.config.mjs
@@ -0,0 +1,39 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { remarkReadingTime } from './src/utils/readTime.ts'
+import { siteConfig } from './src/data/site.config'
+import node from '@astrojs/node'
+import clerk from '@clerk/astro'
+
+// https://astro.build/config
+export default defineConfig({
+	site: siteConfig.site,
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		}
+	},
+
+	adapter: node({ mode: 'standalone' }),
+	integrations: [
+		clerk(),
+		mdx({
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+				experimentalThemes: {
+					light: 'vitesse-light',
+					dark: 'material-theme-palenight'
+				},
+				wrap: true
+			},
+			drafts: true
+		}),
+		sitemap(),
+		tailwind()
+	]
+})
