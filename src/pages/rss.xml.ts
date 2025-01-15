cbb11eda95586bea9afb8fac026ae8011b6e17d9
3urdparty <3urdparty@gmail.com>
2025-01-15 23:58:06 +0800

    Added UI Components for tailwindui

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,14 +3,14 @@ import { getCollection } from 'astro:content'
 import { siteConfig } from '@/site-config'
 
 export async function GET(context: any) {
-	const posts = await getCollection('blog')
+	const tours = await getCollection('tours')
 	return rss({
 		title: siteConfig.title,
 		description: siteConfig.description,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `post/${post.slug}/`
+		items: tours.map((tour) => ({
+			...tour.data,
+			link: `tour/${tour.slug}/`
 		}))
 	})
 }
